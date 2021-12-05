63c2dc3f78004f24e8d13ad62e642a3504ab40eb
Shubham Patil <shubhampatilsd@gmail.com>
2021-12-04 22:31:14 -0800

    problem five done!

--- five/p_two/main.py
+++ five/p_two/main.py
@@ -1,3 +1,7 @@
+def pointExists(slope, b, x, y) -> bool:
+    return y == (slope*x)+b
+
+
 fileInput = open("input.txt", "r")
 fileInput = fileInput.readlines()
 for line in range(0, len(fileInput)):
@@ -10,39 +14,42 @@ for coordinates in fileInput:
         " -> ")[0].split(",")[0] == coordinates.split(" -> ")[1].split(",")[0]
     horizontal = coordinates.split(
         " -> ")[0].split(",")[1] == coordinates.split(" -> ")[1].split(",")[1]
-    # if(horizontal):
-    #     y = int(coordinates.split(" -> ")[0].split(",")[1])
-    #     x1 = int(coordinates.split(" -> ")[0].split(",")[0])
-    #     x2 = int(coordinates.split(" -> ")[1].split(",")[0])
-    #     for i in range(min(x1, x2), max(x1, x2)+1):
-    #         if(not points.get(f"{i}:{y}")):
-    #             points[f"{i}:{y}"] = 1
-    #         else:
-    #             # change to more conscisce
-    #             points[f"{i}:{y}"] = points[f"{i}:{y}"]+1
-    # elif vertical:
-    #     x = int(coordinates.split(" -> ")[0].split(",")[0])
-    #     y1 = int(coordinates.split(" -> ")[0].split(",")[1])
-    #     y2 = int(coordinates.split(" -> ")[1].split(",")[1])
-    #     for i in range(min(y1, y2), max(y1, y2)+1):
-    #         if(not points.get(f"{x}:{i}")):
-    #             points[f"{x}:{i}"] = 1
-    #         else:
-    #             # change to more conscisce
-    #             points[f"{x}:{i}"] = points[f"{x}:{i}"]+1
-    x1 = int(coordinates.split(" -> ")[0].split(",")[0])
-    x2 = int(coordinates.split(" -> ")[1].split(",")[0])
-    y1 = int(coordinates.split(" -> ")[0].split(",")[1])
-    y2 = int(coordinates.split(" -> ")[1].split(",")[1])
-
-    for x in range(min(x1, x2), max(x1, x2)+1):
-        for y in range(min(y1, y2), max(y1, y2)+1):
-            if(not points.get(f"{x}:{y}")):
-                points[f"{x}:{y}"] = 1
+    if(horizontal):
+        y = int(coordinates.split(" -> ")[0].split(",")[1])
+        x1 = int(coordinates.split(" -> ")[0].split(",")[0])
+        x2 = int(coordinates.split(" -> ")[1].split(",")[0])
+        for i in range(min(x1, x2), max(x1, x2)+1):
+            if(not points.get(f"{i}:{y}")):
+                points[f"{i}:{y}"] = 1
             else:
                 # change to more conscisce
-                points[f"{x}:{y}"] = points[f"{x}:{y}"]+1
-
+                points[f"{i}:{y}"] = points[f"{i}:{y}"]+1
+    elif vertical:
+        x = int(coordinates.split(" -> ")[0].split(",")[0])
+        y1 = int(coordinates.split(" -> ")[0].split(",")[1])
+        y2 = int(coordinates.split(" -> ")[1].split(",")[1])
+        for i in range(min(y1, y2), max(y1, y2)+1):
+            if(not points.get(f"{x}:{i}")):
+                points[f"{x}:{i}"] = 1
+            else:
+                # change to more conscisce
+                points[f"{x}:{i}"] = points[f"{x}:{i}"]+1
+    else:
+        x1 = int(coordinates.split(" -> ")[0].split(",")[0])
+        x2 = int(coordinates.split(" -> ")[1].split(",")[0])
+        y1 = int(coordinates.split(" -> ")[0].split(",")[1])
+        y2 = int(coordinates.split(" -> ")[1].split(",")[1])
+        slope = (y1-y2)/(x1-x2)
+        b = y1-(slope*x1)
+        for x in range(min(x1, x2), max(x1, x2)+1):
+            for y in range(min(y1, y2), max(y1, y2)+1):
+                if(not pointExists(slope, b, x, y)):
+                    pass
+                elif(not points.get(f"{x}:{y}")):
+                    points[f"{x}:{y}"] = 1
+                else:
+                    # change to more conscisce
+                    points[f"{x}:{y}"] = points[f"{x}:{y}"]+1
 count = 0
 for coordinate, value in points.items():
     # print(coordinate, value)
