2dbebc86cde5f1f4d52800815e122fb6d6bee81e
Shubham Patil <shubhampatilsd@gmail.com>
2021-12-05 21:12:56 -0800

    p_one done

--- six/main.js
+++ six/main.js
@@ -1,1 +1,18 @@
-const text = require("fs").readFileSync("one.txt").toString();
+let text = require("fs")
+  .readFileSync("input.txt")
+  .toString()
+  .split(",")
+  .map(Number);
+
+for (let i = 1; i <= 80; i++) {
+  const text_length = text.length;
+  for (let fish = 0; fish < text_length; fish++) {
+    if (text[fish] === 0) {
+      text[fish] = 6;
+      text.push(8);
+    } else {
+      text[fish] = text[fish] - 1;
+    }
+  }
+}
+console.log(text.length);
